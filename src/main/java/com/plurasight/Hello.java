cc2f9e6c655126ea970bcd95cbf68caba6d99f6e
amanichristianbisetsa <bisetsachristian@gmail.com>
2026-04-13 13:54:22 -0500

    First commit

---
+++ src/main/java/com/plurasight/Hello.java
@@ -0,0 +1,25 @@
+package com.plurasight;
+
+public class Hello {
+    public static void main(String[] args) {
+        sayHello();
+        sayGoodbye();
+        sayGoodmornig();
+
+    }
+
+    public static void sayHello() {
+        System.out.println("Hello World!");
+
+    }
+
+    public static void sayGoodbye() {
+        System.out.println("Goodbye!");
+
+    }
+
+    public static void sayGoodmornig(){
+        System.out.println("Good Morning!");
+
+    }
+}
