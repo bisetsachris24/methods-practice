542533ebc6e8355eae5c32dd207fc684ddbdcfa0
amanichristianbisetsa <bisetsachristian@gmail.com>
2026-04-13 14:20:25 -0500

    First commit

---
+++ src/main/java/com/plurasight/format.java
@@ -0,0 +1,16 @@
+package com.plurasight;
+
+public class format {
+    public static void main(String[] args) {
+        formatName( "Amani"," Bisetsa", 24);
+
+    }
+
+    public static void formatName(String first, String last, int age ){
+
+        System.out.println(last + "," + first + age);
+
+
+
+    }
+}
